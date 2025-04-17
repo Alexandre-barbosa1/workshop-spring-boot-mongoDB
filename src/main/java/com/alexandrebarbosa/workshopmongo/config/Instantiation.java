3bee27a0669fda80d0873936a035c03ed4c13666
Alexandre <josealexandre2005@outlook.com>
2025-04-17 11:48:53 -0300

    Referenciando os posts do usuario

--- src/main/java/com/alexandrebarbosa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/alexandrebarbosa/workshopmongo/config/Instantiation.java
@@ -23,6 +23,8 @@ public class Instantiation implements CommandLineRunner {
 
     @Autowired
     private PostRepository repPost;
+    @Autowired
+    private UserRepository userRepository;
 
 
     @Override
@@ -40,5 +42,9 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
         Post post2 = new Post (null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
         repPost.saveAll(Arrays.asList(post1,post2));
+
+        maria.getPosts().add(post1);
+        maria.getPosts().add((post2));
+        userRepository.save(maria);
     }
 }
